3f22db6a31aba00a5775de2c8b67da235812a4b6
Prince Kumar Upadhyay <princeupadhyay6264@gmail.com>
2024-12-12 00:58:15 +0530

    updated frontend 2.0

--- frontend/environment.js
+++ frontend/environment.js
@@ -1,11 +1,12 @@
 
-let is_prod = true;
+// let is_prod = true;
 
 
-const server= is_prod ?
- "https://conference-3cu1.onrender.com" :
-    "http://localhost:8000" 
+// const server= is_prod ?
+//  "https://conference-3cu1.onrender.com" :
+//     "http://localhost:8000" 
 
+const server = "https://conference-3cu1.onrender.com" 
 
     console.log(server);
   
